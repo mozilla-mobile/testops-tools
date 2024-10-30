e3c7e68a0377f3e581ac3f461c142d2f8661345c
Clare So <cso@mozilla.com>
2024-10-30 16:41:55 -0400

    Backup Firefox/Focus for both iOS and Android

--- backup-tools/backup_testrail.py
+++ backup-tools/backup_testrail.py
@@ -2,67 +2,34 @@ import json
 import csv
 from datetime import datetime
 from testrail import TestRail
+from pathvalidate import sanitize_filename
 
-PROJECT = {
-    'Fenix Browser': {
-        'id': 59,
-        'suite': {
-            'Full Functional Tests Suite': 3192
-        }
-    },
-    'Firefox for iOS': {
-        'id': 14,
-        'suite': {
-            'Full Functional Tests Suite': 45443,
-            'iOS Performance Test Suite': 1298
-        }
-    },
-    'Focus for iOS': {
-        'id': 27,
-        'suite': {
-            'Full Functional Test Suite': 5291
-        }
-    },
-    'Firefox for Android': {
-        'id': 13,
-        'suite': {
-            'Smoke Tests': 142,
-            'L10N Test suite': 186
-        }
-    },
-    'Focus for Android': {
-        'id': 49,
-        'suite': {
-            'Full Functional Tests Suite': 1028
-        }
-    }
-}
-
-if __name__ == "__main__":
+def create_csv(project_id, project_name, suite_id, suite_name):
+    print("Fetching {project}: {suite}".format(project=project_name, suite=suite_name))
     
-    testrail = TestRail()
     now = datetime.now()
+    testrail = TestRail()
+    response = testrail.test_cases(project_id, suite_id)
     
-    project = 'Firefox for iOS'
-    suite = 'Full Functional Tests Suite'
-    PROJECT_ID = PROJECT.get(project).get('id')
-    SUITE_ID = PROJECT.get(project).get('suite').get(suite)
-
-    response = testrail.test_cases(PROJECT_ID, SUITE_ID)
     # Get only the cases only
-    test_case = response['cases']
+    cases = response['cases']
+    
+    # Do not create backup for empty suites
+    if len(cases) == 0:
+        return
     
     output_json_file = "data-{project}-{suite}-{year}-{month}-{day}.json".format(
-        project=project, 
-        suite=suite,
+        project=project_name, 
+        suite=suite_name,
         year=now.year,
         month=now.month,
         day=now.day
     )
+    output_json_file = sanitize_filename(output_json_file)
     
     # write test cases to json file
     with open(output_json_file, 'w') as output:
-        json.dump(test_case, output, indent=2)
+        json.dump(cases, output, indent=2)
    
     # write test cases to CSV file
     output_csv_file = output_json_file.replace(".json", ".csv")
@@ -72,20 +39,43 @@ if __name__ == "__main__":
 
         # Print header
         # Rearrange the multi-line steps and expected result to be at the rightmost
-        backup_fields = [*test_case[0].keys()]
+        backup_fields = [*cases[0].keys()]
         backup_fields.remove('custom_steps_separated')
         backup_fields.append('Steps')
         backup_fields.append('Expected Result')
         csv_writer.writerow(backup_fields) 
 
         # Print rows (including unravel the Steps and Expected Results)
-        for test in test_case[1:-1]:
-            first_row = [test[field] for field in backup_fields[:-2]] # need treatment for steps
-            first_row.append(test['custom_steps_separated'][0]['content'])
-            first_row.append(test['custom_steps_separated'][0]['expected'])
+        for case in cases[1:-1]:
+            first_row = [case.get(field, '') for field in backup_fields[:-2]] # need treatment for steps
+            steps = case['custom_steps_separated']
+            if steps:
+                first_row.append(steps[0].get('content', ''))
+                first_row.append(steps[0].get('expected', ''))
+            else:
+                first_row.append('')
+                first_row.append('')
+                steps = []
             csv_writer.writerow(first_row)
-            for step in test['custom_steps_separated'][1:-1]:
+            
+            for step in steps[1:-1]:
                 row = ["" for field in backup_fields[:-2]]
                 row.append(step['content'])
                 row.append(step['expected'])
                 csv_writer.writerow(row)
+
+if __name__ == "__main__":
+    testrail = TestRail()
+
+    # Fenix Browser, Firefox for iOS, Focus for iOS, Firefox for Android, Focus for Android
+    # for project_id in [59, 14, 27, 13, 48]:
+    
+    for project_id in [59, 14, 27, 13, 48]:
+        project = testrail.project(project_id)
+        project_name = project.get('name')
+        suites = testrail.test_suites(project_id)
+        
+        for suite in suites:
+            suite_id = suite.get('id')
+            suite_name = suite.get('name')
+            create_csv(project_id, project_name, suite_id, suite_name)
