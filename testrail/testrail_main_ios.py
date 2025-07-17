9a8c027666043a5c849049fb4617ec34a5bd92e4
mbarone <mbarone@mozilla.com>
2025-07-17 12:09:16 +0200

    Change the filter

--- testrail/testrail_main_ios.py
+++ testrail/testrail_main_ios.py
@@ -87,11 +87,9 @@ def main():
         filters = {
             "custom_automation_status": 4, # Automation = Completed
             "custom_automation_coverage": 3, # Automation Coverage = Full
-            "custom_sub_test_suites": lambda v: set(v or []) == {1, 2} # Suite Functional & Smoke&Sanity
+            # "custom_sub_test_suites": lambda v: set(v or []) == {1, 2} # Suite Functional & Smoke&Sanity
+            "custom_sub_test_suites": lambda v: all(x in (v or []) for x in [1, 2])
         }
-
-        print("*********FILTERS***********")
-        print(filter)
 
         case_ids = testrail.get_case_ids_by_multiple_custom_fields(
             testrail_project_id,
